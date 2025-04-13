1f9f420fb88146f363c30513ccaed3f831d0ebd3
Hlebusshek <prokofyeva.anna97@mail.ru>
2025-04-14 01:10:33 +0300

    Римские цифры

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,20 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    const roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+    for (let i = 0; i < str.length; i++) {
+        if (roman[str[i]] < roman[str[i + 1]] && i + 1 != str.length) {
+            result -= roman[str[i]];
+        } else {
+            result += roman[str[i]];
+        }
+    }
     return result;
 }
